858abfe5bc27cd5f5fcf304ed1205a1822ef68a3
Robert Harrison <harrisonrw@gmail.com>
2024-09-13 10:53:13 -0700

    Initial Xcode Project

---
+++ ImageGallery/ImageGallery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageGallery
+//
+//  Created by Robert Harrison on 9/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageGallery/ImageGallery/ImageGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGalleryApp.swift
+//  ImageGallery
+//
+//  Created by Robert Harrison on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
