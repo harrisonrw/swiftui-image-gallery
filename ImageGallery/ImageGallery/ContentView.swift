290c6f67454d62ca2c81f25fd5b8c0acc9e465ac
Robert Harrison <harrisonrw@gmail.com>
2024-09-27 14:10:18 -0700

    Grid of square images (SF Symbols)

--- ImageGallery/ImageGallery/ContentView.swift
+++ ImageGallery/ImageGallery/ContentView.swift
@@ -8,12 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let images = ["car", "bus", "tram", "cablecar", "truck.box", "sailboat", "ferry", "airplane", "bicycle", "scooter"]
+
+    private let cellSize: CGFloat = 100
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: cellSize, maximum: .infinity), spacing: 2)], spacing: 2) {
+                   ForEach(images, id: \.self) { image in
+                       Image(systemName: image)
+                           .font(.system(size: 48))
+                           .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                           .aspectRatio(1, contentMode: .fit)
+                           .background(Color.indigo)
+                           .foregroundStyle(.white)
+                   }
+               }
+           }
         }
         .padding()
     }
